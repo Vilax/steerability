3d rotation fixed

util/filter3d.py
@@ -5,15 +5,17 @@ Created on Thu Oct 17 16:03:22 2019
 @author: tommy
 """
 import numpy as np
+from numpy import linalg as npla
+import util.steering3D as ste
 
 
-def make_filter(filtSize, r0, sigma_denom, f, phi):
-    filter_range = np.arange(-filtSize, filtSize, 1)
+def make_filter(n, r0, sigma_denom, f, phi):
+    filter_range = np.arange(-n, n+1, 1)
     # filter_range = np.array(np.arange(-n,n+1))
     print('filter_range.shape = ', filter_range.shape)
     X, Y, Z = np.meshgrid(filter_range, filter_range, filter_range)
     R = np.sqrt(X**2 + Y**2 + Z**2)
-    sigma = filtSize / sigma_denom
+    sigma = n / sigma_denom
     
     # g = np.exp(-np.power(R-r0,2) / (2*(np.power(sigma,2))))
     g = np.ones(X.shape)
@@ -33,7 +35,56 @@ def make_filter(filtSize, r0, sigma_denom, f, phi):
     filt = np.multiply(g, spherical_vals)
     return filt
 
+def makeOrientedFilter3d(n, f, phi, orientation, radial_type, *args):
+    filter_range = np.arange(-n, n+1, 1)
+    X, Y, Z = np.meshgrid(filter_range, filter_range, filter_range)
+    R = np.sqrt(X**2 + Y**2 + Z**2)
     
+    north_pole = [0,0,1]
+    orientation = orientation/npla.norm(orientation)
+    rotMat = ste.get_rotation_matrix(orientation, north_pole)
+    
+    num_coordinates = np.size(R)
+    coordinates = np.concatenate((X.reshape(num_coordinates,1),\
+                            Y.reshape(num_coordinates,1),\
+                            Z.reshape(num_coordinates,1)), axis = 1)
+    
+    rotCoordinates = np.dot(rotMat,coordinates.transpose()).transpose()
+    rotCoordinates = np.array(rotCoordinates)
+    xrot = np.reshape(rotCoordinates[:,0], X.shape)
+    yrot = np.reshape(rotCoordinates[:,1], Y.shape)
+    zrot = np.reshape(rotCoordinates[:,2], Z.shape)
+    u, v, w = project_to_sphere(X, Y, Z, R)
+    
+    angles = np.arccos(w)
+    a = np.reshape(angles, (np.size(angles), 1))
+    phi = np.reshape(phi, np.size(f))
+    values = np.interp( np.ndarray.flatten(a), phi, \
+                       np.ndarray.flatten(np.array(f)))
+    spherical_vals = np.reshape(values, angles.shape)
+    
+    params = args
+    g = get_radial_function(n, radial_type, params)
+    filt = np.multiply(g, spherical_vals)
+    return filt
+    
+def get_radial_function(n,radial_type, params):
+    params = params[0]
+    if radial_type is 'gaussian':
+        r0 = params[0]
+        sigma = params[1]
+        g = makeGaussianRadial3d(n,r0,sigma)
+    elif radial_type is 'spline':
+        pass
+    return g
+
+def makeGaussianRadial3d(n,r0,sigma):
+    filter_range = np.arange(-n, n+1, 1)
+    X, Y, Z = np.meshgrid(filter_range, filter_range, filter_range)
+    R = np.sqrt(X**2 + Y**2 + Z**2)  
+    g = np.exp(-np.power(R-r0,2) / (2*(np.power(sigma,2))))
+    return g
+
 def project_to_sphere(X, Y, Z, R):
     SMALL_CONSTANT = 1e-8
     R = R+SMALL_CONSTANT

util/steering3D.py
@@ -5,7 +5,7 @@ from scipy.spatial.transform import Rotation as R
 from numpy import linalg as la
 from scipy import ndimage as ndimg
 import util.polynomial3d as poly3d
-import util.filter3d as filt
+import util.filter3d as filt3d
 import util.utilFuntions as uf
 
 
@@ -18,17 +18,19 @@ def directionalFilter3D(N, cap, r0, sigma, direction, filtSize):
     import time
 
     f, v, bCos, phi = poly3d.steerable_polynomial3d(cap, N)
-    filtSize = int(filtSize) / 2  # in pixels
+    n = int(np.floor(filtSize / 2))  # in pixels
+    print(n)
     start = time.time()
-    orig_filt = filt.make_filter(filtSize, r0, sigma, f, phi)
+    orig_filt = filt3d.make_filter(n, r0, sigma, f, phi)
     end = time.time()
     print('Total Time ', end - start)
-    img = orig_filt[:, 100, :]
+    img = orig_filt[:, n, :]
     uf.representImg(img, 'Filter', True)
 
-    direction_cosines = np.loadtxt('3d_direction_cosines_N4.txt')
+    direction_cosines = np.loadtxt('scripts/3d_direction_cosines_N4.txt')
     start = time.time()
-    steer_basis_hypervol = make_steer_basis3d(orig_filt, direction_cosines)
+    steer_basis_hypervol = make_steer_basis3d(n, f, phi, direction_cosines,\
+                                              'gaussian', r0, sigma)
     end = time.time()
     print('Total Time ', end - start)
     num_basis_filters = steer_basis_hypervol.shape[3]
@@ -208,16 +210,19 @@ def direction_cosine_powers3d(N):
     return powers
 
 
-def make_steer_basis3d(filt, steer_direction_array):
+def make_steer_basis3d(n, f, phi, steer_direction_array, radial_type, *args):
     nfilt = steer_direction_array.shape[0]
-    nrows, ncols, nframes = filt.shape
-
+    nrows = int(2*n+1)
+    ncols = nrows
+    nframes = nrows
     steer_filt_hypervolume = np.zeros((nrows,ncols,nframes,nfilt))
 
     for ifilt in np.arange(nfilt):
         steer_direction = steer_direction_array[ifilt,:]
-        steer_filt = rotate_filter3d(filt, steer_direction)
+        steer_filt = filt3d.makeOrientedFilter3d(n,f,phi, steer_direction, \
+                                          radial_type, args)
         steer_filt_hypervolume[:,:,:,ifilt] = steer_filt
+
         
     return steer_filt_hypervolume
 